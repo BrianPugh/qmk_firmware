e658a6deb36fcbf85a7303200ebb0f59fdd08346
Brian Pugh <bnp117@gmail.com>
2026-01-06 20:29:44 -0500

    al68 bpugh: use grave escape, relocate EE_CLR to backspace on layer 2
      - Esc → QK_GESC: Shift+Esc sends ~, GUI+Esc sends `
      - Layer 2: EE_CLR moved to backspace position
      - Add KC_GRAV for shift-insensitive grave on layer 2

--- keyboards/yunzii/al68/keymaps/bpugh/keymap.c
+++ keyboards/yunzii/al68/keymaps/bpugh/keymap.c
@@ -3,6 +3,7 @@
 enum my_keycodes {
     KC_PDB = SAFE_RANGE,
     KC_MPL,
+    KC_GRAV,  // Grave that ignores shift
 };
 
 #define KC_WINDOW_LEFT LCTL(KC_LEFT)
@@ -20,7 +21,7 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Layer 0: Default
      * ,----------------------------------------------------------------.
-     * |Esc| 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 0 | - | = |Bksp |Mute|
+     * |GEs| 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 0 | - | = |Bksp |Mute|
      * |----------------------------------------------------------------|
      * |Tab  | Q | W | E | R | T | Y | U | I | O | P | [ | ] |  \  |Del |
      * |----------------------------------------------------------------|
@@ -32,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `----------------------------------------------------------------'
      */
     [0] = LAYOUT_65_ansi_blocker(
-        KC_ESC,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_MUTE,
+        QK_GESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_MUTE,
         KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL,
         MO(1),    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGUP,
         KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_PGDN,
@@ -62,7 +63,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     /* Layer 2: BLE & Backlight (held from right of space)
      * ,----------------------------------------------------------------.
-     * |EEC| F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12| Del |    |
+     * | ` | F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12| EEC |    |
      * |----------------------------------------------------------------|
      * |     |BL1|BL2|BL3|24G|   |   |   |   |   |   |   |   |     |    |
      * |----------------------------------------------------------------|
@@ -74,7 +75,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `----------------------------------------------------------------'
      */
     [2] = LAYOUT_65_ansi_blocker(
-        EE_CLR,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  _______,
+        KC_GRAV, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  EE_CLR,  _______,
         _______, KC_BLE1, KC_BLE2, KC_BLE3, KC_24G,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          RM_VALU, _______,
@@ -114,6 +115,14 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                     SS_TAP(X_LEFT)
                     SS_TAP(X_LEFT)
                 );
+            }
+            return false;
+        case KC_GRAV:
+            if (record->event.pressed) {
+                uint8_t mods = get_mods();
+                del_mods(MOD_MASK_SHIFT);
+                tap_code(KC_GRV);
+                set_mods(mods);
             }
             return false;
     }
