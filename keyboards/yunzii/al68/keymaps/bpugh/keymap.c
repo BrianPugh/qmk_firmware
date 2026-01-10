11011135a1b047d7b91826b2e54b57fe10f7d5ee
Brian Pugh <bnp117@gmail.com>
2026-01-10 11:29:49 -0500

    deskhop macros

--- keyboards/yunzii/al68/keymaps/bpugh/keymap.c
+++ keyboards/yunzii/al68/keymaps/bpugh/keymap.c
@@ -4,11 +4,14 @@ enum my_keycodes {
     KC_PDB = SAFE_RANGE,
     KC_MPL,
     KC_GRAV,  // Grave that ignores shift
+    KC_DH_CO,  // DeskHop Config Open
+    KC_DH_GAME,  // DeskHop Gaming Mode
 };
 
 #define KC_WINDOW_LEFT LCTL(KC_LEFT)
 #define KC_WINDOW_RIGHT LCTL(KC_RIGHT)
 #define KC_WINDOW_UP LCTL(KC_UP)
+#define KC_DH_SW LCTL(KC_CAPS)  // DeskHop Switch outputs
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
@@ -23,9 +26,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ,----------------------------------------------------------------.
      * |GEs| 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 0 | - | = |Bksp |Mute|
      * |----------------------------------------------------------------|
-     * |Tab  | Q | W | E | R | T | Y | U | I | O | P | [ | ] |  \  |Del |
+     * |Tab  | Q | W | E | R | T | Y | U | I | O | P | [ | ] |  \  |DhSw|
      * |----------------------------------------------------------------|
-     * |Fn    | A | S | D | F | G | H | J | K | L | ; | ' | Enter |PgUp|
+     * |Fn    | A | S | D | F | G | H | J | K | L | ; | ' | Enter |DhGm|
      * |----------------------------------------------------------------|
      * |Shift   | Z | X | C | V | B | N | M | , | . | / |Shift| Up |PgDn|
      * |----------------------------------------------------------------|
@@ -34,8 +37,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
     [0] = LAYOUT_65_ansi_blocker(
         QK_GESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_MUTE,
-        KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL,
-        MO(1),    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGUP,
+        KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_DH_SW,
+        MO(1),    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_DH_GAME,
         KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_PGDN,
         KC_LCTL,  KC_LGUI, KC_LALT,                   KC_SPC,                    MO(2),   KC_RCTL,                   KC_LEFT, KC_DOWN, KC_RGHT
     ),
@@ -44,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ,----------------------------------------------------------------.
      * | ` | F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12| Del |    |
      * |----------------------------------------------------------------|
-     * |     |Prv|Ply|Nxt|Rbt|Dbg|   |PgU| Up|PgD|PDB|   |   |     |Ins |
+     * |     |Prv|Ply|Nxt|Rbt|Dbg|   |PgU| Up|PgD|PDB|   |   |     |DhCO|
      * |----------------------------------------------------------------|
      * |      |   |VoD|VoU|Mut|   |Hom|Lft|Dwn|Rgt|Ins|Del| Enter |WinR|
      * |----------------------------------------------------------------|
@@ -55,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
     [1] = LAYOUT_65_ansi_blocker(
         KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  _______,
-        _______, KC_MPRV, KC_MPLY, KC_MNXT, QK_RBT,  DB_TOGG, _______, KC_PGUP, KC_UP,   KC_PGDN, KC_PDB,  _______, _______, _______, KC_INS,
+        _______, KC_MPRV, KC_MPLY, KC_MNXT, QK_RBT,  DB_TOGG, _______, KC_PGUP, KC_UP,   KC_PGDN, KC_PDB,  _______, _______, _______, KC_DH_CO,
         _______, _______, KC_VOLD, KC_VOLU, KC_MUTE, _______, KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT, KC_INS,  KC_DEL,           _______, KC_WINDOW_RIGHT,
         KC_CAPS, _______, KC_BRID, KC_BRIU, _______, QK_BOOT, KC_END,  KC_MPL,  _______, _______, _______, _______,          KC_WINDOW_UP, KC_WINDOW_LEFT,
         _______, _______, _______,                   _______,                   _______, _______,                   _______, _______, _______
@@ -130,6 +133,32 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 del_mods(MOD_MASK_SHIFT);
                 tap_code(KC_GRV);
                 set_mods(mods);
+            }
+            return false;
+        case KC_DH_CO:
+            // DeskHop needs all keys in the same HID report
+            if (record->event.pressed) {
+                add_mods(MOD_BIT(KC_LCTL) | MOD_BIT(KC_RSFT));
+                add_key(KC_C);
+                add_key(KC_O);
+                send_keyboard_report();
+                wait_ms(50);
+                del_key(KC_O);
+                del_key(KC_C);
+                del_mods(MOD_BIT(KC_LCTL) | MOD_BIT(KC_RSFT));
+                send_keyboard_report();
+            }
+            return false;
+        case KC_DH_GAME:
+            // DeskHop needs all keys in the same HID report
+            if (record->event.pressed) {
+                add_mods(MOD_BIT(KC_LCTL) | MOD_BIT(KC_RSFT));
+                add_key(KC_G);
+                send_keyboard_report();
+                wait_ms(50);
+                del_key(KC_G);
+                del_mods(MOD_BIT(KC_LCTL) | MOD_BIT(KC_RSFT));
+                send_keyboard_report();
             }
             return false;
     }
